9e37526b149501fa12e38e0361df2b7fcecab000
woshitieren <woshitieren@gmail.com>
2017-05-05 17:23:35 +0800

    first commit

---
+++ maopao.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+
+li = [13,22,6,99,11]
+print li
+for m in range(4):
+    num1 = li[m]
+    num2 = li[m+1]
+if num1 > num2:
+        temp = li[m]
+        li[m] = num2
+        li[m+1] = temp
+print li
+for m in range(3):
+    num1 = li[m]
+    num2 = li[m+1]
+if num1 > num2:
+        temp = li[m]
+        li[m] = num2
+        li[m+1] = temp
+print li
+for m in range(2):
+    num1 = li[m]
+    num2 = li[m+1]
+if num1 > num2:
+        temp = li[m]
+        li[m] = num2
+        li[m+1] = temp
+print li
+
+
+
+
+简化通过for循环
+
+#!/usr/bin/env python
+#coding:utf-8
+
+
+li = [13,22,6,99,11]
+
+for n in range(1,len(li)):
+#1,2,3,4
+for m in range(len(li)-n):  #4,3,2,1
+num1 = li[m]
+        num2 = li[m+1]
+if num1 > num2:
+            temp = li[m]
+            li[m] = num2
+            li[m+1] = temp
+print li
